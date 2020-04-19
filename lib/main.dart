7beabca311e6b0fed1389f33a0cedd53653ee577
emreerkmen <emre.erkmen@hotmail.com>
2020-04-19 17:41:04 +0300

    magic-8-ball challenge init

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Center(
+              child: Text('New App'),
+            ),
+          ),
+        ),
       ),
     );
